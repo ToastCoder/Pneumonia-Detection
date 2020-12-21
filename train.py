85664292617e1c5a36e580dfd273a5266ea15be3
ToastCoder <vicky.pcbasic@gmail.com>
2020-12-21 19:13:27 +0530

    Added initial nn config

--- train.py
+++ train.py
@@ -1,1 +1,38 @@
+# IMPORTING REQUIRED LIBRARIES
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# DATA PREPROCESSING
+
+dir_train = './dataset/train'
+datagen_train = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255.)
+train_data = datagen_train.flow_from_directory(dir_train,
+                                               target_size = (200,200), 
+                                               color_mode = 'grayscale', 
+                                               class_mode = 'binary', 
+                                               batch_size = 10)
+dir_test = './dataset/test'
+datagen_test = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255.)
+test_data = datagen_test.flow_from_directory(dir_test,
+                                               target_size = (200,200), 
+                                               color_mode = 'grayscale', 
+                                               class_mode = 'binary', 
+                                               batch_size = 10)
+
+# DEFINING NEURAL NETWORK
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Conv2D(filters = 32, kernel_size = 3, activation = 'relu', input_shape = (200,200,1)))
+model.add(tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2))
+model.add(tf.keras.layers.Conv2D(filters = 32, kernel_size = 3, activation = 'relu'))
+model.add(tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128, activation = 'relu'))
+model.add(tf.keras.layers.Dense(1, activation = 'sigmoid'))
+
+# FITTING AND TRAINING THE MODEL
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+model.fit(train_data, validation_data = test_data, epochs = 10)
+
 
