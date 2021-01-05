770e88d52bfd6d932644721d1d0844e46ed59c68
ToastCoder <vicky.pcbasic@gmail.com>
2021-01-05 23:02:11 +0530

    Some changes

--- train.py
+++ train.py
@@ -1,32 +1,57 @@
+# PNEUMONIA DETECTION
+
+# FILE NAME: train.py
+
+# DEVELOPED BY: Vigneshwar Ravichandar
+
+# TOPICS: Binary Classification, Deep Learning, TensorFlow, Convolutional Neural Networks
+
+# DISABLE TF DEBUGGING INFORMATION
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+print("TensorFlow Debugging Information is hidden.")
+
 # IMPORTING REQUIRED LIBRARIES
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+print(f"TensorFlow version: {tf.__version__}")
+
+DIR_TRAIN = './dataset/train'
+DIR_TEST = './dataset/test'
+ACC_THRESHOLD = 0.99
+MODEL_PATH = './model/pneumonia-model'
+
 # DATA PREPROCESSING
-dir_train = './dataset/train'
 datagen_train = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255.)
-train_data = datagen_train.flow_from_directory(dir_train,
-                                               target_size = (200,200), 
-                                               color_mode = 'grayscale', 
-                                               class_mode = 'binary', 
-                                               batch_size = 10)
-dir_test = './dataset/test'
-datagen_test = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255.)
-test_data = datagen_test.flow_from_directory(dir_test,
+train_data = datagen_train.flow_from_directory(DIR_TRAIN,
                                                target_size = (200,200), 
                                                color_mode = 'grayscale', 
                                                class_mode = 'binary', 
                                                batch_size = 10)
 
-# DEFINING NEURAL NETWORK
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Conv2D(filters = 32, kernel_size = 3, activation = 'relu', input_shape = (200,200,1)))
-model.add(tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2))
-model.add(tf.keras.layers.Conv2D(filters = 32, kernel_size = 3, activation = 'relu'))
-model.add(tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2))
-model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(128, activation = 'relu'))
-model.add(tf.keras.layers.Dense(1, activation = 'sigmoid'))
+datagen_test = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255.)
+test_data = datagen_test.flow_from_directory(DIR_TEST,
+                                               target_size = (200,200), 
+                                               color_mode = 'grayscale', 
+                                               class_mode = 'binary', 
+                                               batch_size = 10)
+
+# DEFINING NEURAL NETWORK FUNCTION
+def pneumoniaModel():
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D(filters = 32, kernel_size = 3, activation = 'relu', input_shape = (200,200,1)))
+    model.add(tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2))
+    model.add(tf.keras.layers.Dropout(rate= 0.25))
+    model.add(tf.keras.layers.Conv2D(filters = 64, kernel_size = 3, activation = 'relu'))
+    model.add(tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2))
+    model.add(tf.keras.layers.Dropout(rate= 0.25))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(128, activation = 'relu'))
+    model.add(tf.keras.layers.Dense(1, activation = 'sigmoid'))
+    return model
+
+model = pneumoniaModel()
 
 # FITTING AND TRAINING THE MODEL
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
@@ -42,6 +67,6 @@ plt.legend(['train', 'validation'], loc='upper left')
 plt.show()
 
 # SAVING THE MODEL
-tf.keras.models.save_model(model,'./model/pneumonia-model')
+tf.keras.models.save_model(model,MODEL_PATH)
 
 
