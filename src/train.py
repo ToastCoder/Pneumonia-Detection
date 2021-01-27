3bf5ed11a9fc61d0b494a7ee2c83d8a9029367ec
ToastCoder <vicky.pcbasic@gmail.com>
2021-01-27 10:32:43 +0530

    Some changes

--- src/train.py
+++ src/train.py
@@ -6,48 +6,62 @@
 
 # TOPICS: Binary Classification, Deep Learning, TensorFlow, Convolutional Neural Networks
 
-# DISABLE TF DEBUGGING INFORMATION
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-print("TensorFlow Debugging Information is hidden.")
-
 # IMPORTING REQUIRED LIBRARIES
+import os
 import matplotlib.pyplot as plt
+import matplotlib.image as im
 import tensorflow as tf
 
 print(f"TensorFlow version: {tf.__version__}")
 
-DIR_TRAIN = './dataset/train'
-DIR_TEST = './dataset/test'
+DIR_TRAIN = './dataset/pneumonia_extended/train'
+DIR_TEST = './dataset/pneumonia_extended/test'
+
+DIR_TRAIN_NORMAL = './dataset/pneumonia_extended/train/NORMAL'
+DIR_TRAIN_PNEUMONIA = './dataset/pneumonia_extended/train/PNEUMONIA'
+DIR_TEST_NORMAL = './dataset/pneumonia_extended/test/NORMAL'
+DIR_TEST_PNEUMONIA = './dataset/pneumonia_extended/test/PNEUMONIA'
+
 ACC_THRESHOLD = 0.99
 MODEL_PATH = './model/pneumonia-model'
 
 # DATA PREPROCESSING
 datagen_train = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255.)
 train_data = datagen_train.flow_from_directory(DIR_TRAIN,
-                                               target_size = (200,200), 
+                                               target_size = (150,150), 
                                                color_mode = 'grayscale', 
                                                class_mode = 'binary', 
                                                batch_size = 10)
 
 datagen_test = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255.)
 test_data = datagen_test.flow_from_directory(DIR_TEST,
-                                               target_size = (200,200), 
+                                               target_size = (150,150), 
                                                color_mode = 'grayscale', 
                                                class_mode = 'binary', 
                                                batch_size = 10)
 
+
+# COUNTING NUMBER OF IMAGE SAMPLES 
+print('Total normal images on Train set :', len(os.listdir(DIR_TRAIN_NORMAL)))
+print('Total pneumonia images on Train set :', len(os.listdir(DIR_TRAIN_PNEUMONIA)))
+print('Total normal images on Test set :', len(os.listdir(DIR_TEST_NORMAL)))
+print('Total pneumonia images on Test set :', len(os.listdir(DIR_TEST_PNEUMONIA)))
+
+
 # DEFINING NEURAL NETWORK FUNCTION
 def pneumoniaModel():
     model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Conv2D(filters = 32, kernel_size = 3, activation = 'relu', input_shape = (200,200,1)))
-    model.add(tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2))
+    model.add(tf.keras.layers.Conv2D(filters = 16, kernel_size = (3,3), activation = 'relu', input_shape = (150,150,1)))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size =(2,2)))
     model.add(tf.keras.layers.Dropout(rate= 0.25))
-    model.add(tf.keras.layers.Conv2D(filters = 64, kernel_size = 3, activation = 'relu'))
-    model.add(tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2))
+    model.add(tf.keras.layers.Conv2D(filters = 32, kernel_size = (3,3), activation = 'relu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size = (2,2)))
+    model.add(tf.keras.layers.Dropout(rate= 0.25))
+    model.add(tf.keras.layers.Conv2D(filters = 64, kernel_size = (3,3), activation = 'relu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size = (2,2)))
     model.add(tf.keras.layers.Dropout(rate= 0.25))
     model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(128, activation = 'relu'))
+    model.add(tf.keras.layers.Dense(512, activation = 'relu'))
     model.add(tf.keras.layers.Dense(1, activation = 'sigmoid'))
     return model
 
@@ -60,6 +74,8 @@ class Callback(tf.keras.callbacks.Callback):
 
 model = pneumoniaModel()
 callback = Callback()
+
+model.summary()
 
 # FITTING AND TRAINING THE MODEL
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
